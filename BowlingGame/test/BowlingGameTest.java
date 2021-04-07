e6fd75ac8d3527c8466172c321f957ec25be5413
KEHP-K <rnjsxovnd@gmail.com>
2021-04-07 18:22:18 +0900

    second test 2

--- BowlingGame/test/BowlingGameTest.java
+++ BowlingGame/test/BowlingGameTest.java
@@ -13,19 +13,20 @@ public class BowlingGameTest {
 	
 	@Test
 	public void testGullterGame() {
-		for(int i=0; i<20; i++) {
-			g.roll(0);
-		}
+		rollMany(20,0);
 		assertEquals(0, g.score());
 	}
 	
 
 	@Test
 	public void testAllOneGame() {
-		for(int i=0; i<20; i++) {
-			g.roll(1);
-		}
+		rollMany(20,1);
 		assertEquals(20, g.score());
 	}
-
+	
+	protected void rollMany(int n, int pins) {
+		for(int i=0; i<20; i++) {
+			g.roll(pins);
+		}
+	}
 }
