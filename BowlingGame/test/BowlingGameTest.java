5e6bdc8f62b1c32571ac4d3ce429ab2fbfa5efda
KEHP-K <rnjsxovnd@gmail.com>
2021-04-07 18:44:25 +0900

    third test 4

--- BowlingGame/test/BowlingGameTest.java
+++ BowlingGame/test/BowlingGameTest.java
@@ -26,11 +26,15 @@ public class BowlingGameTest {
 
 	@Test
 	public void testOneSpare() {
-		g.roll(5);
-		g.roll(5);
+		rollSpare();
 		g.roll(3);
 		rollMany(17,0);
 		assertEquals(16, g.score());
+	}
+	
+	private void rollSpare() {
+		g.roll(5);
+		g.roll(5);
 	}
 	
 	protected void rollMany(int n, int pins) {
